3ba850da17ab76cd5070744b0877a17cc2eeae8d
Rodrigo Losik <rodrigolosik@gmail.com>
2023-06-26 18:23:40 +0100

    Value and Reference types and Boxing and Unboxing

---
+++ src/BoxingUnboxing.Console/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String Chicken = "chicken";
+        int Fish = 1;
+
+        ArrayList Truck = new ArrayList
+        {
+            Chicken,
+            Fish
+        };
+        string chick = (string)Truck[0];
+        int fish = (int)Truck[1];
+
+        Console.ReadKey();
+    }
+}
+
+
+abstract class User
+{
+    public string Username { get; set; }
+
+    public abstract void GetPrivileges();
+    public string GetName()
+    {
+        return Username;
+    }
+}
+
+class AdministrativeUser : User
+{
+    public override void GetPrivileges()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class RegularUser : User
+{
+    public override void GetPrivileges()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/ValueAndReferenceTypes.Console/Program.cs
@@ -0,0 +1,76 @@
+﻿internal class Program
+{
+    /// <summary>
+    /// Value types 
+    /// Value species lives on STACK
+    /// - bool
+    /// - byte
+    /// - char
+    /// - decimal
+    /// - double
+    /// - enum
+    /// - float
+    /// - int
+    /// - long
+    /// - sbyte
+    /// - short
+    /// - struct
+    /// - uint
+    /// - ulong
+    /// - ushort
+    /// 
+    /// Reference Types
+    /// Reference species lives on the HEAP
+    /// - String
+    /// - Arrays
+    /// - Class
+    /// - Delegates
+    /// 
+    /// </summary>
+
+    private static void Main(string[] args)
+    {
+        //int i = 100;
+        //Console.WriteLine(i);
+
+        //ChangeValue(i);
+        //Console.WriteLine(i);
+
+        //Student std1 = new Student();
+        //std1.StudentName = "Bill";
+        //ChangeReferenceType(std1);
+        //Console.WriteLine(std1.StudentName);
+
+        String stringOne = "StringOne";
+        string stringTwo = "StringTwo";
+
+        Console.WriteLine("Before: {0}, {1}", stringOne, stringTwo);
+
+        ChangeStringValues(stringOne, stringTwo);
+
+        Console.WriteLine("After: {0}, {1}", stringOne, stringTwo);
+
+    }
+
+    static void ChangeStringValues(String stringOne, string stringTwo)
+    {
+        stringOne = "NewStringOne";
+        stringTwo = "NewStringTwo";
+    }
+
+    static void ChangeValue(int x)
+    {
+        x = 200;
+        Console.WriteLine(x);
+    }
+
+    static void ChangeReferenceType(Student std2)
+    {
+        std2.StudentName = "Steve";
+    }
+}
+
+internal class Student
+{
+    public string StudentName { get; set; }
+}
