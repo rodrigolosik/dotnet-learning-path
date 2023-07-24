bb343088dab1232f06a0bde438345625bbd27edb
Rodrigo Losik <rodrigolosik@gmail.com>
2023-07-24 12:13:18 +0100

    Flags

---
+++ src/Flags.Console/Cor.cs
@@ -0,0 +1,60 @@
+﻿namespace Flags.Console;
+
+public enum CorSemFlag // enumerador comum
+{
+    Vermelho = 1,
+    Verde = 2,
+    Azul = 4,
+    Amarelo = 8
+}
+
+[Flags]
+public enum Cor // usando flags, basta adicionar o atributo
+{
+    Vermelho = 1,
+    Verde = 2,
+    Azul = 4,
+    Amarelo = 8
+}
+
+
+// declaração errada com valores padrões
+//[Flags]
+//public enum Cor
+//{
+//    Vermelho, // valor padrão 0
+//    Verde,    // valor padrão 1
+//    Azul,     // valor padrão 2
+//    Amarelo   // valor padrão 3
+//}
+
+//// declaração errada com atribuição de valores não sequênciais na base 2^x
+//[Flags]
+//public enum Cor
+//{
+//    Vermelho = 1,
+//    Verde = 2,
+//    Azul = 3,
+//    Amarelo = 4
+//}
+
+//// declaração correta
+//[Flags]
+//public enum Cor
+//{
+//    Vermelho = 1,
+//    Verde = 2,
+//    Azul = 4,
+//    Amarelo = 8
+//}
+
+//// declaração correta, usando bit-shifting 
+//[Flags]
+//public enum Cor
+//{
+//    None = 0,
+//    Vermelho = 1 << 0,
+//    Verde = 1 << 1,
+//    Azul = 1 << 2,
+//    Amarelo = 1 << 3
+//}
---
+++ src/Flags.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using Flags.Console;
+
+CorSemFlag minhasCores = CorSemFlag.Vermelho | CorSemFlag.Verde | CorSemFlag.Azul;
+Cor minhasCoresFlags = Cor.Vermelho | Cor.Verde | Cor.Azul;
+
+Console.WriteLine(minhasCores);
+Console.WriteLine(minhasCoresFlags);
+
+Cor novasCores = Enum.Parse<Cor>("Vermelho, Verde, Azul"); // Funcionará e irá atribuir corretamente os valores
+
+var cores = Cor.Verde | Cor.Vermelho;
+
+var resultado1 = cores.HasFlag(Cor.Verde); // true
+
+var resultado2 = cores.HasFlag(Cor.Vermelho); // true
+
+var resultado3 = cores.HasFlag(Cor.Amarelo); // false
+
+var resultado4 = cores.HasFlag(Cor.Verde | Cor.Vermelho); // true, pois contém os dois
+
+var resultado5 = cores.HasFlag(Cor.Verde) && cores.HasFlag(Cor.Vermelho); // true, mesma coisa que a expressão acima
+
+var resultado6 = cores.HasFlag(Cor.Verde | Cor.Amarelo); // false, pois um elemento não está no conjunto
+
+var resultado7 = cores.HasFlag(Cor.Verde) || cores.HasFlag(Cor.Amarelo); // true, por que contém pelo menos um
+
+
+// Para .NET 4 (também funcionará para as versões mais atualizadas, porém a "escrita" é "pior"
+
+var resultado8 = ((cores & Cor.Verde) == Cor.Verde); // true, pois a cor Verde está em cores 
