40a4ed79e526c633f8492335c0495b65ba3254d0
Rodrigo Losik <rodrigolosik@gmail.com>
2023-07-29 15:54:30 +0100

    Strategy Design Pattern

---
+++ src/DesignPatterns.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using DesignPatterns.Behavioral.Strategy;
+
+HairCut hairCut = new(new TexturedCrop());
+hairCut.DoHairCut();
+
+hairCut.SetHairCut(new Caesar());
+hairCut.DoHairCut();
+
+hairCut.SetHairCut(new FlatTop());
+hairCut.DoHairCut();
+
+Console.ReadKey();
